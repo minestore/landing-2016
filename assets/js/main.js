96d2c27f79bfd29f290d0d6f3d86dd2bae9faf4e
Pedro Belli <pedro.belli2@gmail.com>
2016-07-27 16:53:29 -0300

    Added download ebook form functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,6 @@ $( document ).ready(function() {
     event.preventDefault();
 
     createStore($('#email').val());
-
   });
 
   $('#create-store-box').on("click", function(event){
@@ -25,5 +24,42 @@ $( document ).ready(function() {
       window.location.href = "https://app.minestore.com.br/criar-loja?email=" + data.email;
     });
   }
-  
+
+  $('#download-header').on("click", function(event){
+    event.preventDefault();
+
+    downloadEbook($('#email-header').val());
+  });
+
+  $('#download-footer').on("click", function(event){
+    event.preventDefault();
+
+    downloadEbook($('#email-footer').val());
+  });
+
+  function downloadEbook(email) {
+    var payload = {
+      email: email,
+      type: 'ebook-divulgar-facebook'
+    }
+
+    $.post("https://app.minestore.com.br/leads.json", payload)
+    .done(function(data) {
+        $('#cta-header-form').fadeOut();
+        setTimeout(function() {
+          $('#success-cta-header').fadeIn();
+        }, 400);
+
+        $('#cta-footer-form').fadeOut();
+        setTimeout(function() {
+          $('#success-cta-footer').fadeIn();
+        }, 400);
+    });
+  }
+
+  $('.create-store-header').on("click", function(event){
+    event.preventDefault();
+
+    window.location.href = "https://app.minestore.com.br/criar-loja";
+  });
 });
